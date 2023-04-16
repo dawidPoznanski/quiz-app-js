bf7754a1a7f0c388af85042d0faa89bce3e82fa3
Dawid Poznański <dawid.poznanski@outlook.com>
2023-04-16 22:10:17 +0200

    save scores in localStorage

--- js/end.js
+++ js/end.js
@@ -3,14 +3,28 @@ const saveScoreBtn = document.getElementById('save-score-btn');
 const finalScore = document.getElementById('final-score');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
 
+const highScores = JSON.parse(localStorage.getItem('highScore')) || [];
+const MAX_HIGH_SCORES = 5;
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener('keyup', () => {
-  console.log(username.value);
   saveScoreBtn.disabled = !username.value;
 });
 
 saveHighScore = (e) => {
   console.log('clicked');
   e.preventDefault();
+  const score = {
+    score: mostRecentScore,
+    name: username.value,
+  };
+  highScores.push(score);
+
+  highScores.sort((a, b) => b.score - a.score);
+
+  highScores.splice(5);
+
+  localStorage.setItem('highScore', JSON.stringify(highScores));
+  window.location.assign('/');
 };
